d2cfd1b6ba7c19d55c04add8c64d84b26427c2d4
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-08-03 10:55:55 +0100

    move comments to relevant places as reminder

--- spec/fec/fec_spec.rb
+++ spec/fec/fec_spec.rb
@@ -5,26 +5,24 @@
 require 'spec_helper'
 require 'fec'
 
-#Fec.fec_free
-#Fec.fec_new
-#Fec.fec_decode
-#Fec.fec_encode
-
 module Fec
 	describe "fec_new" do
 		it "should create a new fec object"
+		#Fec.fec_new
 	end
 
 	describe "fec_free" do
 		it "should free a fec object"
+		#Fec.fec_free
 	end
 
 	describe "fec_encode" do
 		it "encode data"
+		#Fec.fec_encode
 	end
 
 	describe "fec_decode" do
 		it "decode data"
+		#Fec.fec_decode
 	end
-
 end
